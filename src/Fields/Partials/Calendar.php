a2c2b608dcc4d3e794bcefe8d21c70c871239081
Jayson Garcia <garciajayson502@gmail.com>
2025-08-12 17:27:06 +0800

    refactor: replaced load_field filter with dynamic extracted fields so the acf ajax will working properly like relationship, post object, etc...

--- src/Fields/Partials/Calendar.php
+++ src/Fields/Partials/Calendar.php
@@ -2,38 +2,26 @@
 
 namespace FinancialCalendar\Fields\Partials;
 
+use Log1x\AcfComposer\Partial;
 use StoutLogic\AcfBuilder\FieldsBuilder;
-use FinancialCalendar\Providers\FinancialCalendarServiceProvider;
 
-class Calendar
+class FinancialCalendar extends Partial
 {
-    protected $fields;
-
-    public function __construct()
-    {
-        $parent_layout_key = FinancialCalendarServiceProvider::$parent_layout_key;
-        $subfield_key = 'calendar';
-        $final_key = $parent_layout_key . '_' . $subfield_key;
-
-        $this->fields = new FieldsBuilder($final_key);
-
-        $this->fields
-            ->addText('title', [
-                'label' => 'Title',
-                'instructions' => 'Please enter a title to appear above the financial calendar.',
-                'required' => 0,
-                'maxlength' => false,
-                '_name' => 'title',
-            ]);
-    }
-
     /**
-     * Get the built fields for the partial
+     * The partial field group.
      *
      * @return array
      */
-    public function register(): array
+    public function fields()
     {
-        return $this->fields->build();
+        $Fields = new FieldsBuilder('calendar');
+
+        $Fields
+            ->addText('title', [
+                'label' => 'Title',
+                'instructions' => 'Please enter a title to appear above the financial calendar.',
+            ]);
+
+        return $Fields;
     }
 }
--- src/Providers/FinancialCalendarServiceProvider.php
+++ src/Providers/FinancialCalendarServiceProvider.php
@@ -7,8 +7,6 @@ use FinancialCalendar\Fields\Calendar as CalendarField;
 use FinancialCalendar\Fields\Partials\Calendar as CalendarBuilderField;
 use FinancialCalendar\View\Composers\Calendar as CalendarComposer;
 use Log1x\AcfComposer\AcfComposer;
-use StoutLogic\AcfBuilder\FieldsBuilder;
-use App\Fields\Partials\Clones\Builder;
 
 class FinancialCalendarServiceProvider implements Provider
 {
@@ -22,59 +20,56 @@ class FinancialCalendarServiceProvider implements Provider
         ];
     }
 
+    protected function registerFields()
+    {
+        $composer = app(AcfComposer::class);
+        $calendar = new CalendarField($composer);
+        $calendar->compose();
+    }
+
+    protected function registerLayouts()
+    {
+        add_filter('acf_page_builder_before_build', function ($builder) {
+            $fields = $builder->getFields();
+
+            $flexible = null;
+
+            foreach ($fields as $field) {
+                if ($field->getName() === 'builder') {
+                    $flexible = $field;
+                    break;
+                }
+            }
+
+            if ($flexible) {
+                $composer = app(AcfComposer::class);
+
+                $flexible
+                    ->addLayout((new CalendarBuilderField($composer))->fields(), [
+                        'label' => 'Calendar',
+                        'display' => 'block',
+                    ]);
+            }
+
+            return $builder;
+        });
+    }
+
     public function register()
     {
-        $calendar_key = 'calendar';  // Variable value
-        self::$parent_layout_key = 'builder_builder_' . $calendar_key;
-
         foreach ($this->providers() as $service) {
             (new $service)->register();
         }
+
+        $this->registerFields();
+        $this->registerLayouts();
     }
 
     public function boot()
     {
-        // Register Fields
-        $composer = app(AcfComposer::class);
-        $calendar = new CalendarField($composer);
-        $calendar->compose();
-
-        add_filter('acf/load_field/name=builder', function ($field) {
-            if (isset($field['layouts'])) {
-                $calendar_builder = new CalendarBuilderField();
-
-                $layout = [
-                    'key' => 'field_'.self::$parent_layout_key,
-                    'name' => 'calendar',
-                    'label' => 'Calendar',
-                    'display' => 'block',
-                    'sub_fields' => $calendar_builder->register()['fields'],
-                    'min' => '',
-                    'max' => '',
-                    'acfe_flexible_render_template' => false,
-                    'acfe_flexible_render_style' => false,
-                    'acfe_flexible_render_script' => false,
-                    'acfe_flexible_thumbnail' => false,
-                    'acfe_flexible_settings' => false,
-                    'acfe_flexible_settings_size' => 'medium',
-                    'acfe_flexible_modal_edit_size' => false,
-                    'acfe_flexible_category' => false,
-                ];
-
-                $field['layouts'][] = $layout;
-            }
-            // dd($field);
-            return $field;
-        });
-
         // Register views
-        if ( function_exists( '\Roots\view' ) ) {
-            \Roots\view()->addNamespace('FinancialCalendar', dirname(dirname(__DIR__)) . '/resources/views/');
-
-            // \Roots\view('FinancialCalendar::partials.builder.calendar', ['test' => 'variable'])->render();
-        }
+        View::addLocation(dirname(dirname(__DIR__)) . '/resources/views');
 
         View::composer('FinancialCalendar::partials.builder.calendar', CalendarComposer::class);
-
     }
 }
